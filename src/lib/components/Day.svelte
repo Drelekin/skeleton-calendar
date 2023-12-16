0724ca8f2b89c84607ba2e7151bdc9f6dccf397c
Indrek <leede.indrek@gmail.com>
2023-12-16 10:06:36 +0200

    Calendar layout

---
+++ src/lib/components/Day.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let date: number = 0;
+	export let background: string = '';
+</script>
+
+<button
+	on:click
+	class="bg-cover rounded-container-token"
+	style="background-image: url({background});"
+>
+	<h3 class="p-4 text-lg drop-shadow md:text-xl xl:text-2xl 2xl:text-4xl">{date}</h3>
+</button>
